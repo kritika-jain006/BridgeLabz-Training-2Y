87d37a8ea42cfc72f573f7f2d5d7867f1423ad2f
Kritika Jain <jkritika375@gmail.com>
2025-08-19 12:13:17 +0530

    Kilometers to Miles

---
+++ Day1_ProgrammingElements/02Java Programming/Kilometers.java
@@ -0,0 +1,8 @@
+public class Kilometers {
+    public static void main(String[] args) {
+        double kilometers = 10.8;
+        double miles = kilometers * 0.621371;
+        System.out.println("The distance "+kilometers+"km in miles is "+ miles);
+    }
+}
+
