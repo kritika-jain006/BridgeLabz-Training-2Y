62b840ab58e06293bef9896b5969416b5223dfed
Kritika Jain <jkritika375@gmail.com>
2025-08-23 20:00:57 +0530

    Largest or not

---
+++ Day1_ProgrammingElements/Java Control Flow Level 1/Largest.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Largest {
+    
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if (a > b && a > c) {
+            System.out.println("Is the first number the largest? Yes");
+        } else {
+            System.out.println("Is the first number the largest? No");
+        }
+
+        if (b > a && b > c) {
+            System.out.println("Is the second number the largest? Yes");
+        } else {
+            System.out.println("Is the second number the largest? No");
+        }
+
+        if (c > a && c > b) {
+            System.out.println("Is the third number the largest? Yes");
+        } else {
+            System.out.println("Is the third number the largest? No");
+        }
+        
+        sc.close();   
+}
+}
