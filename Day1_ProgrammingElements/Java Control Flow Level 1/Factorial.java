c4931cf64a7b808a5f6f9bca75e9f77bd070db3e
Kritika Jain <jkritika375@gmail.com>
2025-08-23 20:37:09 +0530

    Factorial

---
+++ Day1_ProgrammingElements/Java Control Flow Level 1/Factorial.java
@@ -0,0 +1,16 @@
+import java.util.*;    
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int fact = 1;
+        
+        while(n >= 1) {
+            fact *= n;
+            n--;
+        }   
+        
+        System.out.println(fact);
+        sc.close();
+    }
+}
