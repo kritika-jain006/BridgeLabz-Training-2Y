081f213d1566b3ed02870b5e82bfaeeff39ee6ba
Kritika Jain <jkritika375@gmail.com>
2025-08-24 22:31:12 +0530

    Leap Year

---
+++ Day1_ProgrammingElements/Java Control Flow Level 3/LeapYear.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class LeapYear{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = sc.nextInt();
+        if (year < 1582) {
+            System.out.println("Year must be 1582 or later.");
+        } else if (year % 4 != 0) {
+            System.out.println("Not a Leap Year");
+        } else if (year % 100 != 0) {
+            System.out.println("Leap Year");
+        } else if (year % 400 == 0) {
+            System.out.println("Leap Year");
+        } else {
+            System.out.println("Not a Leap Year");
+        }
+        if (year >= 1582 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))) {
+            System.out.println("Leap Year (Logical check)");
+        } else if (year >= 1582) {
+            System.out.println("Not a Leap Year (Logical check)");
+        }
+        sc.close();
+    }
+}
