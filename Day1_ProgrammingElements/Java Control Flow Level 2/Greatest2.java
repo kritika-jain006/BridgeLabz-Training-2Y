5f9b8c4cd7e73873d747e8970a2df24358a0bbfd
Kritika Jain <jkritika375@gmail.com>
2025-08-24 15:36:56 +0530

    Greatest 2

---
+++ Day1_ProgrammingElements/Java Control Flow Level 2/Greatest2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Greatest2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int greatestFactor = 1;
+        int counter = number - 1;
+
+        while (counter >= 1) {
+            if (number % counter == 0) {
+                greatestFactor = counter;
+                break;
+            }
+            counter--;
+        }
+
+        System.out.println("Number: " + number + ", Greatest Factor: " + greatestFactor);
+        sc.close();
+    }
+}
