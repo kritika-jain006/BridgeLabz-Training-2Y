b58d2bfe1c7623ebf929bbc7b63dc6eaeba1e0f7
Kritika Jain <jkritika375@gmail.com>
2025-08-24 22:58:35 +0530

    Count

---
+++ Day1_ProgrammingElements/Java Control Flow Level 3/Count.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Count {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter an integer: ");
+    int number = sc.nextInt();
+    int count = 0;
+    int temp = Math.abs(number);
+    if (temp == 0) {
+        count = 1;
+    } else {
+        while (temp != 0) {
+            temp /= 10;
+            count++;
+        }
+    }
+    System.out.println("Number of digits: " + count);
+    sc.close();
+    }
+}
