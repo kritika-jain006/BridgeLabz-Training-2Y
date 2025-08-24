6e3aab146aeee2a36ec1d32349940d3e128e4382
Kritika Jain <jkritika375@gmail.com>
2025-08-24 23:09:06 +0530

    Switch1

---
+++ Day1_ProgrammingElements/Java Control Flow Level 3/Switch1.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Switch1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double first = sc.nextDouble();
+        System.out.print("Enter second number: ");
+        double second = sc.nextDouble();
+        System.out.print("Enter operator (+, -, *, /): ");
+        String op = sc.next();
+
+        System.out.print("Enter number of times to run: ");
+        int number = sc.nextInt();
+
+        for (int i = 1; i < number; i++) {
+            switch (op) {
+                case "+":
+                    System.out.println("Result: " + (first + second));
+                    break;
+                case "-":
+                    System.out.println("Result: " + (first - second));
+                    break;
+                case "*":
+                    System.out.println("Result: " + (first * second));
+                    break;
+                case "/":
+                    if (second != 0) {
+                        System.out.println("Result: " + (first / second));
+                    } else {
+                        System.out.println("Cannot divide by zero.");
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid Operator");
+            }
+        }
+        sc.close();
+    }
+}
