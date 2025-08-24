100cd76a973b5a52c8bdb9116be888030ccc396a
Kritika Jain <jkritika375@gmail.com>
2025-08-24 15:24:09 +0530

    Factor 2

---
+++ Day1_ProgrammingElements/Java Control Flow Level 2/Factor2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Factor2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int number = sc.nextInt();
+        if (number > 0) {
+            int counter = 1;
+            System.out.println("Factors of " + number + " are:");
+            while (counter <= number) {
+                if (number % counter == 0) {
+                    System.out.println(counter);
+                }
+                counter++;
+            }
+        } else {
+            System.out.println("Please enter a positive integer.");
+        }
+        sc.close();
+    }
+}
