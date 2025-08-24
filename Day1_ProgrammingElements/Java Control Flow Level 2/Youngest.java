b7251b46cee91cc929e84004022156e955053a94
Kritika Jain <jkritika375@gmail.com>
2025-08-24 15:02:42 +0530

    Youngest

---
+++ Day1_ProgrammingElements/Java Control Flow Level 2/Youngest.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class Youngest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter age of Amar: ");
+        int ageAmar = sc.nextInt();
+        System.out.print("Enter height of Amar: ");
+        int heightAmar = sc.nextInt();
+
+        System.out.print("Enter age of Akbar: ");
+        int ageAkbar = sc.nextInt();
+        System.out.print("Enter height of Akbar: ");
+        int heightAkbar = sc.nextInt();
+
+        System.out.print("Enter age of Anthony: ");
+        int ageAnthony = sc.nextInt();
+        System.out.print("Enter height of Anthony: ");
+        int heightAnthony = sc.nextInt();
+        int minAge = ageAmar;
+        String youngest = "Amar";
+        if (ageAkbar < minAge) {
+            minAge = ageAkbar;
+            youngest = "Akbar";
+        }
+        if (ageAnthony < minAge) {
+            minAge = ageAnthony;
+            youngest = "Anthony";
+        }
+        int maxHeight = heightAmar;
+        String tallest = "Amar";
+        if (heightAkbar > maxHeight) {
+            maxHeight = heightAkbar;
+            tallest = "Akbar";
+        }
+        if (heightAnthony > maxHeight) {
+            maxHeight = heightAnthony;
+            tallest = "Anthony";
+        }
+
+        System.out.println("The youngest friend is: " + youngest);
+        System.out.println("The tallest friend is: " + tallest);
+        sc.close();
+    }
+}
