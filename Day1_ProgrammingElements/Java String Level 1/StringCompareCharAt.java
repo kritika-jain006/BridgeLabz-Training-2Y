957f3014117fa5a94a44eb45f38773abecb96fd9
Kritika Jain <jkritika375@gmail.com>
2025-10-19 00:09:06 +0530

    Compare

---
+++ Day1_ProgrammingElements/Java String Level 1/StringCompareCharAt.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class StringCompareCharAt {
+
+    
+    public static boolean compareStringsCharAt(String str1, String str2) {
+        if (str1.length() != str2.length()) return false;
+
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1.charAt(i) != str2.charAt(i)) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String str1 = sc.next();
+        System.out.print("Enter second string: ");
+        String str2 = sc.next();
+
+        boolean resultCharAt = compareStringsCharAt(str1, str2);
+        boolean resultEquals = str1.equals(str2);
+
+        System.out.println("Comparison using charAt(): " + resultCharAt);
+        System.out.println("Comparison using equals(): " + resultEquals);
+    }
+}
